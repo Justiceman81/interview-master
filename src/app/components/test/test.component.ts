34267028d52daeb0fac4a761cf4e8e8c3a513903
Justiceman81 <Justiceman814@gmail.com>
2025-01-05 13:30:10 +0200

    creation @Component test

---
+++ src/app/components/test/test.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-test',
+  imports: [],
+  templateUrl: './test.component.html',
+  styleUrl: './_test.component.scss',
+})
+export class TestComponent {}
